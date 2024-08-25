c5df48fa43d94393acba1e40bdf85532ecc26871
Nikita Ranenko <nranenko@bk.ru>
2024-08-25 16:00:19 +0700

    feat: migrations

---
+++ Migrations/20240818133144_CarImage_IsDownloaded_Added.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarsLoader.Migrations
+{
+    /// <inheritdoc />
+    public partial class CarImage_IsDownloaded_Added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDownloaded",
+                table: "Images",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDownloaded",
+                table: "Images");
+        }
+    }
+}
